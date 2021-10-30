4ae5319525b954c6caf16bf9ff12e44bca143c49
Micael <micaelmacedo100@outlook.com>
2021-10-29 23:25:26 -0300

    Criando menu e primeira section

---
+++ view/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Animago</title>
+</head>
+<body>
+    <nav class="menu">
+        <div class="title">
+            <img src="./imagens/logo.jpeg" alt="logo animago">
+        </div>
+        <div class="items-center">
+        <ul>
+            <li><a href="" id="active">HOME</a></li>
+            <li><a href="">SHOP</a></li>
+            <li><a href="">ABOUT</a></li>
+            <li><a href="">GIVING BACK</a></li>
+            <li><a href="">FORUM</a></li>
+            <li><a href="">CONTACT</a></li>
+        </ul>
+        </div>
+        <div class="user-area">
+        <img src="./imagens/shop.png" alt="carinho"><h1>0</h1>
+        <img src="./imagens/user.png" alt="carinho"><h1>Teste</h1>
+        </div>
+    </nav>
+    <section class="home_start">
+        
+    </section>
+</body>
+</html>
