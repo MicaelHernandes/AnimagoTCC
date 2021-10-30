0fd69d1bf9344d0b9f368e7612c8333ec95b82bd
Micael <micaelmacedo100@outlook.com>
2021-10-30 12:41:13 -0300

    Criando footer e seperando navbar em um arquivo aparte

--- view/index.php
+++ view/index.php
@@ -12,33 +12,7 @@
 </head>
 
 <body>
-    <nav class="menu">
-        <div class="title">
-            <img src="./imagens/logo.jpeg" alt="logo animago">
-        </div>
-        <div class="items-center">
-            <ul>
-                <li ><a href="" id="active">HOME</a></li>
-                <li ><a href="">SHOP</a></li>
-                <li ><a href="">PROGRAMA DE PONTOS</a></li>
-                <li ><a href="">SOBRE</a></li>
-                <li ><a href="">FORUM</a></li>
-                <li ><a href="">CONTATO</a></li>
-            </ul>
-        </div>
-        <div class="user-area">
-            <button type="button" class="btn btn-alert m-1">
-                <img src="./imagens/alert.png" alt=""><span class="badge badge-light">4</span>
-            </button>
-            <button type="button" class="btn btn-alert m-1">
-                <img src="./imagens/shop.png" alt=""><span class="badge badge-light">4</span>
-            </button>
-            <button type="button" class="btn btn-alert m-1">
-                <img src="./imagens/user.png" alt=""><span class="badge badge-light">Teste</span>
-            </button>
-
-        </div>
-    </nav>
+    <?php  include_once('../view/menu_nav.php');?>
     <section class="home_start">
         <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
@@ -154,13 +128,34 @@
             </div>
         </div>
     </section>
-    <section class="home-moeda">
+    <section class="home-moeda" style="color: white;">
+        <h1>Moedas</h1>
+        <p>Suas compras são convertidas em Moedas que você pode trocar por descontos no site!</p>
         <img src="./imagens/sobre a moeda.jpeg" alt="">
+        <p><button class="btn btn-primary" style="padding: 15px; margin:10px">Conhecer</button></p>
     </section>
     <section class="footer">
-        
-        
+        <div class="footer-item">
+        <h1>ANIMAGO INC.</h1>
+        <ul style="list-style: none;">
+            <li><a href="">Rações</a></li>
+            <li><a href="">Doações</a></li>
+            <li><a href="">ONGs</a></li>
+            <li><a href="">Sobre</a></li>
+        </ul>
+        </div>
+        <div class="footer-item">
+            <h3>Contato</h3>
+            <p>Telefone: <a href="tel:+551124001234">+55(11)2400-1234</a></p>
+            <p>Celular: <a href="tel:+5511996832330">+55(11)91234-5678</a></p>
+            <p>Email: <a href="mailto:animagoinc@outlook.com">animagoinc@outlook.com</a></p>
+            <p>Endereço: R. Cristóbal Cláudio Elilo, 88 - Parque Cecap, Guarulhos - SP, 07190-065</p>
+        </div>
+        <div class="footer-item">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14640.642975857001!2d-46.5013502!3d-23.4546656!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xedc9be7470ec277c!2sEtec%20de%20Guarulhos!5e0!3m2!1spt-BR!2sbr!4v1635607598149!5m2!1spt-BR!2sbr" width="300" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
     </section>
+    <section class="inc"><h1>By: Grupo Animago! Projeto TCC Etec 2021</h1></section>
 </body>
 
 </html>
---
+++ view/menu_nav.php
@@ -0,0 +1,27 @@
+<nav class="menu">
+        <div class="title">
+            <a href="../view/index.php"><img src="./imagens/logo.jpeg" alt="logo animago"></a>
+        </div>
+        <div class="items-center">
+            <ul>
+                <li ><a href="" id="active">HOME</a></li>
+                <li ><a href="">SHOP</a></li>
+                <li ><a href="">PROGRAMA DE PONTOS</a></li>
+                <li ><a href="">SOBRE</a></li>
+                <li ><a href="">FORUM</a></li>
+                <li ><a href="">CONTATO</a></li>
+            </ul>
+        </div>
+        <div class="user-area">
+            <button type="button" class="btn btn-alert m-1">
+                <img src="./imagens/alert.png" alt=""><span class="badge badge-light">4</span>
+            </button>
+            <button type="button" class="btn btn-alert m-1">
+                <img src="./imagens/shop.png" alt=""><span class="badge badge-light">4</span>
+            </button>
+            <button type="button" class="btn btn-alert m-1">
+                <img src="./imagens/user.png" alt=""><span class="badge badge-light">Teste</span>
+            </button>
+
+        </div>
+    </nav>
