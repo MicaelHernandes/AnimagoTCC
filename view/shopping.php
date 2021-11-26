742037914a537502700720944b8c751289c69bb9
Micael Hernandes (Parzival) <60301307+MicaelHernandes@users.noreply.github.com>
2021-11-25 22:08:24 -0300

    Add files via upload

--- view/shopping.php
+++ view/shopping.php
@@ -1,18 +1,36 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    
+    <link rel="stylesheet" href="./css/shop_ini.css">
     <title>Loja</title>
 </head>
+
 <body>
     <?php
     include_once('./menu_nav.php');
     ?>
-    
+    <section class="home_start">
+        <div class="left_start">
+        <h1>Produtos para seu Pet!</h1>
+        <button class="btn btn-success">conhecer</button>
+        </div>
+        <div class="right_start">
+        <img src="../view/imagens/shoping/aa.png" alt="" width="70%">
+        </div>
+    </section>
+    <section class="home_mid">
+        <img src="../view/imagens/shoping/cachorro_lateral.png" alt="">
+        <img src="../view/imagens/shoping/gato_lateral.png" alt="">
+    </section>
+    <section class="shop-monica">
+        <h1>Sugestões de produtos para seu <span class="text-success">Pet</span></h1>
+    </section>
 </body>
+
 </html>
